118702c226712b6a34c1ae555e37a4d572eda69d
Wolfred Montilla <wolfrednicolas@gmail.com>
2021-04-08 20:03:53 -0400

    add new routes for the landing page, shop, cart and checkout (V2)

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\CheckoutComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,9 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', HomeComponent::class);
+
+Route::get('/shop', ShopComponent::class);
+
+Route::get('/cart', CartComponent::class);
+
+Route::get('/checkout', CheckoutComponent::class);
